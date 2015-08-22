e3c9d1a14ca2c9bca3374086d954d0bb1322015e
khangal <khangaljargal@yahoo.com>
2015-08-22 20:30:00 +0900

    Initial commit

---
+++ database/migrations/2015_08_22_111853_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cv_id')->unsigned();
+            $table->integer('job_id')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('cv_id')
+            ->references('id')->on('cvs');
+            $table->foreign('job_id')
+            ->references('id')->on('jobs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requests');
+    }
+}
