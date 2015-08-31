2e9754f9ab176141568adbcdf77b396229449aed
khangal <khangaljargal@yahoo.com>
2015-08-31 13:44:11 +0900

    Hmmmm

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
 @section('title', 'Бүртгүүлэх')
 
 @section('content')
-<!--<div class="row">
+<div class="row">
     <div class="col-md-4 col-md-offset-4">
         <form method="POST" action="/register">
             {!! csrf_field() !!}
             <h3>Шинээр бүртгэл үүсгэх</h3>
             <input name="email" type="email" class="form-control" placeholder="Е-майл хаяг" required autofocus>
-            <input name="email" type="check-email" class="form-control" placeholder="Ахиад Е-майл хаяг " required>
             <input name="password" type="password" class="form-control" placeholder="Нууц үг" required>
+            <input type="password" class="form-control" name="password_confirmation" placeholder="Нууц үгээ давтан оруулна уу!" required>
             <div class="checkbox">
             <label>
                 <input type="checkbox" name="remember"> Санах
@@ -21,22 +21,6 @@
             <button class="btn btn-lg btn-primary btn-block" type="submit">Бүртгүүлэх</button>
         </form>
     </div>
-</div>-->
-
-<form method="POST" action="/register">
-    {!! csrf_field() !!}
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
+</div>
 
 @endsection
